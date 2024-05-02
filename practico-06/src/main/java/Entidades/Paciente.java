6bbf4223a64d6138516a85e5ed13d9e02db4735b
Ignacio Gutierrez <ij.gutierrez@alumno.um.edu.ar>
2024-05-02 13:47:11 -0300

    Paciente id

--- practico-06/src/main/java/Entidades/Paciente.java
+++ practico-06/src/main/java/Entidades/Paciente.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Paciente {
+    private int id;
     private String nombre;
     private String apellido;
     private String obraSocial;
