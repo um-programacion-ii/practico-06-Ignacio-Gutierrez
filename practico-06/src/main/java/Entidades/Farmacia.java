bea5e2d132caf0af94e2f231aef4ba261e9c1274
Ignacio Gutierrez <ij.gutierrez@alumno.um.edu.ar>
2024-05-02 00:15:07 -0300

    Farmacia entidad

--- practico-06/src/main/java/Entidades/Farmacia.java
+++ practico-06/src/main/java/Entidades/Farmacia.java
@@ -4,11 +4,29 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
+import java.util.NoSuchElementException;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Farmacia {
-    private List<Medicamento> stock;
+    private MedicamentoDaoImpl medicamentoDaoImpl;
+    private Drogueria drogueria;
+
+    public void solicitarMedicamentoPorId(int id, int cantidadNecesaria) {
+        try {
+            medicamentoDaoImpl.retirarCantidadPorId(id, cantidadNecesaria);
+        } catch (NoSuchElementException e) {
+            drogueria.solicitarMedicamentoPorId(id);
+        }
+    }
+
+    public void solicitarMedicamentoPorNombre(String nombre, int cantidadNecesaria) {
+        try {
+            medicamentoDaoImpl.retirarCantidadPorNombre(nombre, cantidadNecesaria);
+        } catch (NoSuchElementException e) {
+            drogueria.solicitarMedicamentoPorNombre(nombre);
+        }
+    }
+
 }
