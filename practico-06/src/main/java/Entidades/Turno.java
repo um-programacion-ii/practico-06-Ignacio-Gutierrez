be2cff572cdf11b81e4f6da93ce817d76099d5b5
Ignacio Gutierrez <ij.gutierrez@alumno.um.edu.ar>
2024-05-02 19:47:01 -0300

    Turno, agregue id y cree la clase de test

--- practico-06/src/main/java/Entidades/Turno.java
+++ practico-06/src/main/java/Entidades/Turno.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Turno {
+    private int id;
     private Paciente paciente;
     private Medico medico;
     private ObraSocial obraSocial;
