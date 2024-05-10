415de02da0b07b11b6890694be07a550ae10272f
Ignacio Gutierrez <ij.gutierrez@alumno.um.edu.ar>
2024-05-09 21:24:50 -0300

    GestionFaramcia + test

--- practico-06/src/main/java/Servicios/GestionFarmaciaService.java
+++ practico-06/src/main/java/Servicios/GestionFarmaciaService.java
@@ -1,12 +1,49 @@
 package Servicios;
 
-public class GestionFarmaciaService {
+import Entidades.*;
 
-    private void iniciarCompra() {
+import java.util.List;
+
+
+public class GestionFarmaciaService {
+    private static GestionFarmaciaService instancia;
+    private ContenedorMemoria contenedorMemoria;
+    private GestionDrogueriaService gestionDrogueriaService;
+
+    private GestionFarmaciaService(ContenedorMemoria contenedorMemoria) {
+        this.contenedorMemoria = contenedorMemoria;
+    }
+
+    public static GestionFarmaciaService getInstancia(ContenedorMemoria contenedorMemoria) {
+        if (instancia == null) {
+            instancia = new GestionFarmaciaService(contenedorMemoria);
+        }
+        return instancia;
+    }
+
+    public void compraDeMedicamentos(Receta receta, Turno turno) {
+        String esadoTurno = turno.getEstadoTurno();
+        List<Medicamento> medicamentos = receta.getMedicamentos();
+        if (esadoTurno == "Finalizada") {
+            for(Medicamento medicamento : medicamentos) {
+                int cantidadARetirar = medicamento.getCantidad();
+                try {
+                    contenedorMemoria.getMedicamentoDao().retirarCantidadPorId(medicamento.getId(), cantidadARetirar);
+                } catch (Exception e) {
+                    System.out.println("No hay suficiente stock de " + medicamento.getNombre());
+                    System.out.println("Solicitando medicamento a drogueria...");
+                    gestionDrogueriaService.getInstancia(contenedorMemoria).solicitarMedicamentoPorId(medicamento.getId());
+
+                    contenedorMemoria.getMedicamentoDao().retirarCantidadPorId(medicamento.getId(), cantidadARetirar);
+
+                }
+            }
+            System.out.println("Compra de medicamentos realizada");
+        } else {
+            throw new RuntimeException("El turno no está finalizado");
+        }
+
         System.out.println("Iniciando compra...");
     }
 
-    private void finalizarCompra() {
-        System.out.println("Finalizando compra...");
-    }
 }
---
+++ practico-06/src/test/java/ServiceTest/GestionFarmaciaSericeTest.java
@@ -0,0 +1,72 @@
+package ServiceTest;
+
+import Dao.Interfaces.*;
+import Entidades.*;
+import Servicios.GestionDrogueriaService;
+import Servicios.GestionFarmaciaService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
+
+public class GestionFarmaciaSericeTest {
+    private GestionFarmaciaService gestionFarmaciaService;
+    private GestionDrogueriaService gestionDrogueriaService;
+    private ContenedorMemoria contenedorMemoria;
+
+    @BeforeEach
+    public void setUp() {
+        contenedorMemoria = Mockito.mock(ContenedorMemoria.class);
+        EspecialidadDAO especialidadDAO = Mockito.mock(EspecialidadDAO.class);
+        TurnoDAO turnoDAO = Mockito.mock(TurnoDAO.class);
+        MedicamentoDAO medicamentoDAO = Mockito.mock(MedicamentoDAO.class);
+        RecetaDAO recetaDAO = Mockito.mock(RecetaDAO.class);
+        Mockito.when(contenedorMemoria.getEspecialidadDao()).thenReturn(especialidadDAO);
+        Mockito.when(contenedorMemoria.getTurnoDao()).thenReturn(turnoDAO);
+        Mockito.when(contenedorMemoria.getMedicamentoDao()).thenReturn(medicamentoDAO);
+        Mockito.when(contenedorMemoria.getRecetaDao()).thenReturn(recetaDAO);
+        gestionFarmaciaService = GestionFarmaciaService.getInstancia(contenedorMemoria);
+    }
+
+
+
+    @Test
+    void getInstanciaTest() {
+        GestionFarmaciaService instancia1 = GestionFarmaciaService.getInstancia(contenedorMemoria);
+        GestionFarmaciaService instancia2 = GestionFarmaciaService.getInstancia(contenedorMemoria);
+
+        assertSame(instancia1, instancia2);
+    }
+
+    @Test
+    public void compraMedicamentosSinIrAlTurnoTest() {
+        Especialidad dermatologia = new Especialidad(1,"Dermatologia");
+
+        ObraSocial osde = new ObraSocial(1,"OSDE");
+        ObraSocial swissMedical = new ObraSocial(2,"Swiss Medical");
+        List<ObraSocial> obrasSocialesAceptadasList = Arrays.asList(osde, swissMedical);
+
+        Medicamento medicamento1 = new Medicamento(1,"Ibuprofeno", 10);
+        Medicamento medicamento2 = new Medicamento(2,"Paracetamol", 5);
+        List<Medicamento> medicamentos = Arrays.asList(medicamento1, medicamento2);
+
+        Paciente paciente = new Paciente(1,"Juan","Perez",osde);
+        Medico medico1 = new Medico(1,"Juan","Perez",dermatologia,obrasSocialesAceptadasList);
+
+        Turno turno1 = new Turno(1, paciente, medico1, false, "Pendiente");
+
+        Receta receta = new Receta(1, medicamentos, medico1, paciente);
+
+        try {
+            gestionFarmaciaService.compraDeMedicamentos(receta, turno1);
+        } catch (Exception e) {
+            assertEquals("El turno no está finalizado", e.getMessage());
+        }
+    }
+
+}
